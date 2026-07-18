45782eadc708b6f3a9217ab7666c94451944ebf3
tukinamiotoko-afk <tukinamiotoko@gmail.com>
2026-07-18 12:47:17 +0900

    Use test ads for Android dev builds

--- src/constants/ads.ts
+++ src/constants/ads.ts
@@ -10,6 +10,8 @@
 // it there.
 import { Platform } from 'react-native';
 
+const USE_ANDROID_TEST_ADS = Platform.OS === 'android' && typeof __DEV__ !== 'undefined' && __DEV__;
+
 export const ADMOB_APP_ID = Platform.select({
   android: 'ca-app-pub-6253728869800176~4424472496',
   ios: 'ca-app-pub-3940256099942544~1458002511',
@@ -17,15 +19,23 @@ export const ADMOB_APP_ID = Platform.select({
 })!;
 
 export const INTERSTITIAL_AD_UNIT_ID = Platform.select({
-  android: 'ca-app-pub-6253728869800176/3925779047',
+  android: USE_ANDROID_TEST_ADS
+    ? 'ca-app-pub-3940256099942544/1033173712'
+    : 'ca-app-pub-6253728869800176/3925779047',
   ios: 'ca-app-pub-3940256099942544/4411468910',
-  default: 'ca-app-pub-6253728869800176/3925779047',
+  default: USE_ANDROID_TEST_ADS
+    ? 'ca-app-pub-3940256099942544/1033173712'
+    : 'ca-app-pub-6253728869800176/3925779047',
 })!;
 
 export const REWARDED_AD_UNIT_ID = Platform.select({
-  android: 'ca-app-pub-6253728869800176/5127180074',
+  android: USE_ANDROID_TEST_ADS
+    ? 'ca-app-pub-3940256099942544/5224354917'
+    : 'ca-app-pub-6253728869800176/5127180074',
   ios: 'ca-app-pub-3940256099942544/1712485313',
-  default: 'ca-app-pub-6253728869800176/5127180074',
+  default: USE_ANDROID_TEST_ADS
+    ? 'ca-app-pub-3940256099942544/5224354917'
+    : 'ca-app-pub-6253728869800176/5127180074',
 })!;
 
 // Show an interstitial after this many tracked actions (task add / complete).
