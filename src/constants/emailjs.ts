d017a169284e88d6c08061136a6e1ae2c2e57763
tukinamiotoko-afk <tukinamiotoko@gmail.com>
2026-07-22 19:55:55 +0900

    Configure EmailJS contact form keys

--- src/constants/emailjs.ts
+++ src/constants/emailjs.ts
@@ -1,5 +1,5 @@
-export const EMAILJS_SERVICE_ID = 'REPLACE_WITH_EMAILJS_SERVICE_ID';
-export const EMAILJS_TEMPLATE_ID = 'REPLACE_WITH_EMAILJS_TEMPLATE_ID';
-export const EMAILJS_PUBLIC_KEY = 'REPLACE_WITH_EMAILJS_PUBLIC_KEY';
+export const EMAILJS_SERVICE_ID = 'service_nsf8tx3';
+export const EMAILJS_TEMPLATE_ID = 'template_rte0jmk';
+export const EMAILJS_PUBLIC_KEY = 'W5Zz9F2_2B5eV2jnY';
 
 export const EMAILJS_ENDPOINT = 'https://api.emailjs.com/api/v1.0/email/send';
